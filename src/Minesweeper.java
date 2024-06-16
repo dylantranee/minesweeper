c52af5a562a6691b317ca4ccf1a99844bb284b07
DylanTran0 <tavan.iuyouth@gmail.com>
2024-06-17 02:28:26 +0700

    add the 'textPanel' to the window

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -5,13 +5,15 @@ import java.util.Random;
 import javax.swing.*;
 
 public class Minesweeper {
-    int tileSize = 24;
+    int tileSize = 48;
     int numRows = 9;
     int numCols = numRows;
     int boardWidth = numCols * tileSize;
     int boardHeight = numRows * tileSize;
 
     JFrame frame = new JFrame("Minesweeper");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
 
     Minesweeper() {
         frame.setVisible(true);
@@ -21,5 +23,13 @@ public class Minesweeper {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
+        textLabel.setFont(new Font("Open Sans", Font.BOLD, 15));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Minesweeper");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
     }
 }
