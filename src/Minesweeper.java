5bf2ba5a6981431490e3e6350af33991ca555d17
DylanTran0 <tavan.iuyouth@gmail.com>
2024-06-17 01:50:06 +0700

    create a window

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,3 +1,25 @@
-public class Minesweeper {
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
 
+public class Minesweeper {
+    int tileSize = 24;
+    int numRows = 9;
+    int numCols = numRows;
+    int boardWidth = numCols * tileSize;
+    int boardHeight = numRows * tileSize;
+
+    JFrame frame = new JFrame("Minesweeper");
+
+    Minesweeper() {
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+    }
 }
